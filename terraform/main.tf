89e6fc208ab7d5d2f3b81021bd6e411e1c40fee1
Jatupat <kooseng2547@gmail.com>
2025-03-06 02:57:09 +0700

    fix:terraformfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 resource "google_compute_instance" "squad_server" {
   project      = "sda-squad-server"
-  name         = "squad-sever"
+  name         = "squad-server"
   machine_type = "e2-standard-2"
   zone         = "asia-southeast1-a"
 
   boot_disk {
     initialize_params {
-      image = "windows-server-2025-datacenter"  
-      size  = 100  # ขนาดดิสก์ 100 GB
-      type  = "pd-balanced"  # แบบของ Persistent Disk ที่เลือกคือ Balanced Persistent Disk
+      image = "windows-server-2025-dc-v20250212"
+      size  = 100  # Disk size 100 GB
+      type  = "pd-balanced"  # Persistent disk type
     }
   }
 
@@ -20,13 +20,32 @@ resource "google_compute_instance" "squad_server" {
   }
 
   metadata = {
-    enable-ip-forwarding = "true"  # เปิด IP Forwarding
+    enable-ip-forwarding = "true"  # Enable IP Forwarding
   }
 
-  # การตั้งค่า Network Firewall Rules สำหรับ Allow HTTP และ HTTPS traffic
-  tags = ["http-server", "https-server"]
+  tags = ["http-server", "https-server"]  # This associates the instance with the firewall rules
+}
 
-  # เปิด HTTP และ HTTPS traffic
-  allow_http = true
-  allow_https = true
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_tags = ["http-server"]  # Allows HTTP traffic to instances with the "http-server" tag
+}
+
+resource "google_compute_firewall" "allow_https" {
+  name    = "allow-https"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_tags = ["https-server"]  # Allows HTTPS traffic to instances with the "https-server" tag
 }
